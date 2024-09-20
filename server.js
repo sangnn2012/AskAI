68089873b1eeec332e64b5872cb7d53fa55ba345
Sang Nguyen <sangnn2012@gmail.com>
2024-09-21 04:00:49 +0700

    feature: create server for app

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const envConfig = require('dotenv').config()
+// const envConfig = require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
@@ -7,5 +7,36 @@ const port = 3000
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
+
+const { Configuration, OpenAIApi } = require('openai');
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+});
+
+const openai = new OpenAIApi(configuration);
+
+app.post('/chat', async (req, res) => {
+    const { messages } = req.body;
+
+    try {
+        if (messages == null) {
+            throw new Error('We have a problem - no prompt was provided');
+        }
+
+        const response = await openai.createChatCompletion({
+            model: 'gpt-3.5-turbo',
+            messages,
+        })
+
+        const completion = response.data.choices[0].message;
+
+        return res.status(200).json({
+            success: true,
+            data: completion
+        })
+    } catch (error) {
+        console.log(error.message);
+    }
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
