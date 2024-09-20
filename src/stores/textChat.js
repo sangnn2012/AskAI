4faa67c5b4ba97b1381e97399d8d13da1aa90da5
Sang Nguyen <sangnn2012@gmail.com>
2024-09-21 04:01:03 +0700

    feature: implement chat feature for app

--- src/stores/textChat.js
+++ src/stores/textChat.js
@@ -1,5 +1,51 @@
-import { defineStore } from 'pinia'
+import { ref } from 'vue';
+import { defineStore } from 'pinia';
 
 export const useTextChatStore = defineStore('textChat', () => {
-  return {}
+  const text = ref('')
+  const question = ref('')
+  const prompt = ref([]);
+  const gptResponse = ref('')
+
+  function createPrompt() {
+    const instructions = {
+      role: 'system',
+      content: 'You will answer a question about the following text.'
+    }
+    const textToAnalyze = { role: 'user', content: text.value }
+    const chatQuestion = { role: 'user', content: question.value }
+
+    prompt.value = [
+      instructions,
+      textToAnalyze,
+      chatQuestion,
+    ]
+  }
+  function sendPrompt() {
+    if (text.value.length === 0) {
+      alert('You have not added any text to analyse.')
+    } else {
+      fetch('http://localhost:3000/chat', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ messages: prompt.value })
+      })
+        .then(response => response.json())
+        .then(data => {
+          gptResponse.value = data.data
+        })
+        .catch(error => console.error(error))
+      }
+  }
+  function clearChat() {
+    text.value = ''
+    question.value = ''
+    prompt.value = []
+    gptResponse.value = ''
+  }
+  return {
+    text, question, prompt, gptResponse, createPrompt, sendPrompt, clearChat
+  }
 })
